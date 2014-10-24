fa5776a38b8b58b933095128fdc5aaca64edd4dd
alex-cory <alexcoryregister@gmail.com>
2014-10-24 02:57:26 -0700

    updating recurring mailer

---
+++ auto_mailer_just_before.php
@@ -0,0 +1,10 @@
+<?php
+// from: (http://php.net/manual/en/function.mail.php)
+$to      = 'alex@hackingedu.co';
+$subject = 'Reminder: HackingEDU Meeting [Core Team]';
+$message = "Hey everyone!\r\n\r\nDon't forget about the meeting in an hour! Excited to see everyone!\r\n\r\nAlex";
+$headers = 'From: alex@alexcory.com' . "\r\n" .
+    'Reply-To: alex@alexcory.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
--- auto_mailer_morning.php
+++ auto_mailer_morning.php
@@ -1,12 +1,10 @@
 <?php
 // from: (http://php.net/manual/en/function.mail.php)
-$to      = 'alex@hackingedu.co';
+$to      = 'alex@hackingedu.co, alexlcory@gmail.com, alexcoryregister@gmail.com';
 $subject = 'Reminder: HackingEDU Meeting [Core Team]';
-$message = "Hey everyone!\n\nDon't forget about the meeting tonight at 9:30pm!  Excited to see everyone! :) \n\nAlex";
+$message = "Hey everyone!\r\n\r\nDon't forget about the meeting tonight at 9:30pm! Excited to see everyone!\r\n\r\nAlex";
 $headers = 'From: alex@alexcory.com' . "\r\n" .
     'Reply-To: alex@alexcory.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-
 mail($to, $subject, $message, $headers);
-?>
