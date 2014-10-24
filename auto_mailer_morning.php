b316d571631052c4b765e87e9eb824365ddb6da4
alex-cory <alexcoryregister@gmail.com>
2014-10-24 03:16:53 -0700

    updating recurring mailer

--- auto_mailer_morning.php
+++ auto_mailer_morning.php
@@ -1,16 +1,17 @@
 <?php
 // from: (http://php.net/manual/en/function.mail.php)
 $to = array(
-	'alex@hackingedu.co',
-	'alexlcory@gmail.com',
-	'alexcoryregister@gmail.com'
+	'AlexHackingEDU <alex@hackingedu.co>',
+	'AlexLCory <alexlcory@gmail.com>',
+	'AlexCoryRegister <alexcoryregister@gmail.com>'
 	);
 $subject = 'Reminder: HackingEDU Meeting [Core Team]';
-$message = "Hey everyone!\r\n\r\nDon't forget about the meeting tonight at 9:30pm! Excited to see everyone! :)\r\n\r\nAlex";
-$headers = 'From: alex@alexcory.com' . "\r\n" .
-    'Reply-To: alex@alexcory.com' . "\r\n" .
+
+$headers = 'From: alex@hackingedu.co' . "\r\n" .
+    'Reply-To: alex@hackingedu.co' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-foreach ($to as $t) {
-	mail($t, $subject, $message, $headers);
+foreach ($to as $k => $v) {
+	$message = "Hey $k!\r\n\r\nDon't forget about the meeting tonight at 9:30pm! Excited to see you there! :)\r\n\r\nAlex";
+	mail($v, $subject, $message, $headers);
 }
