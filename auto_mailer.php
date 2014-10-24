1faa0e466517ba6ea09fb64884577d84c4dc7f36
alex-cory <alexcoryregister@gmail.com>
2014-10-24 00:23:25 -0700

    updating recurring mailer

--- auto_mailer.php
+++ auto_mailer.php
@@ -2,9 +2,9 @@
 // from: (http://php.net/manual/en/function.mail.php)
 $to      = 'alex@hackingedu.co';
 $subject = 'Reminder: HackingEDU Meeting [Core Team]';
-$message = "Hey everyone!\r\n
+$message = "Hey everyone!\n
 
-Don't forget about the meeting tonight at 9:30pm!  Excited to see everyone! \r\n
+Don't forget about the meeting tonight at 9:30pm!  Excited to see everyone! :) \r\n
 
 Alex";
 $headers = 'From: alex@alexcory.com' . "\r\n" .
