65b6872fd791830cb5156a4143db57639be689e7
alex-cory <alexcoryregister@gmail.com>
2014-11-14 04:06:03 -0800

    updated automailer

--- auto_mailer_just_before.php
+++ auto_mailer_just_before.php
@@ -2,7 +2,7 @@
 // from: (http://php.net/manual/en/function.mail.php)
 $to = 'Blake <blake589t@gmail.com>,
 Blake <blake@hackingedu.co>,
-Catherine <cmullings@stanford.edu>,
+Catherine <cmulling@stanford.edu>,
 Catherine <catherine@hackingedu.co,
 Jessica <ballet.jessica@gmail.com>,
 Jessica <jessicaallen@hackingedu.co>,
@@ -12,7 +12,11 @@ Kirill <kirill.satanovsky@gmail.com>,
 Kirill <kirill@hackingedu.co>,
 Yanny <yzhang1130@gmail.com>,
 Yanny <yanny@hackingedu.co>,
-Alex <alex@hackingedu.co>';
+Alex <alex@hackingedu.co>,
+Delian <petrovd@uci.edu>,
+Esther <estherdu@gmail.com>,
+Ellie <myungjikim91@gmail.com>,
+Calvin <calvin@hackingedu.co>';
 $subject = 'Reminder: HackingEDU Meeting [Core Team]';
 $message = "Hey everyone!\r\n\r\nDon't forget about the meeting in an hour! Excited to see everyone! :)\r\n\r\nAlex";
 $headers = 'From: alex@alexcory.com' . "\r\n" .
